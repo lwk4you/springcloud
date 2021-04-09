00e07b5061f4989dd7b395f25287bfef6c413542
lwk4you <lwkloveslife@gmail.com>
2021-04-09 17:10:30 +0800

    练习用springcloudmodule提交，涉及nacos,sentinel,seata

--- cloud-account-service8006/src/main/java/com/lwk4you/springcloud/controller/AccountController.java
+++ cloud-account-service8006/src/main/java/com/lwk4you/springcloud/controller/AccountController.java
@@ -1,26 +1,48 @@
 package com.lwk4you.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.lwk4you.springcloud.entities.AccountTbl;
 import com.lwk4you.springcloud.service.AccountService;
 import com.lwk4you.springcloud.vo.CommonResult;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class AccountController {
     @Autowired
     private AccountService accountService;
 
     @PostMapping("account/decreaseMoney")
+    @SentinelResource(value = "account/decreaseMoney",blockHandler = "fall_back")
     public CommonResult<Object> decreaseMoney(String userId,Integer money){
+
+        log.info("调用账户余额减少接口");
+        accountService.decreaseMoney(userId,money);
+        log.info("调用账户余额减少接口暂停end");
         try {
-            Thread.sleep(6000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        accountService.decreaseMoney(userId,money);
         return new CommonResult<>("200","账户余额减少成功",null);
     }
 
 
-}
+    public CommonResult<Object> fall_back(String userId, Integer money, BlockException e) throws BlockException {
+        log.info("调用账户余额服务降级"+"\t"+e.getStackTrace());
+        accountService.save(new AccountTbl(null,"lwk3",111111));
+        throw e;
+        //return new CommonResult<>("300","服务降级成功",null);
+
+    }
+
+
+
+
+    }
--- cloud-common/src/main/java/com/lwk4you/springcloud/feign/service/OrderService.java
+++ cloud-common/src/main/java/com/lwk4you/springcloud/feign/service/OrderService.java
@@ -2,9 +2,7 @@ package com.lwk4you.springcloud.feign.service;
 
 import com.lwk4you.springcloud.entities.OrderTbl;
 import com.lwk4you.springcloud.vo.CommonResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
